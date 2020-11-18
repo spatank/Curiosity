784ba3221d573450c2c75c90d40bfef6b66d25e2
Shubhankar <sppatankar@ucdavis.edu>
2020-11-18 12:08:50 -0500

    Update compute_compressibility.m

--- compute_compressibility.m
+++ compute_compressibility.m
@@ -2,5 +2,5 @@ clc; close all; clear;
 
 %% Compressibility for Growing Knowledge Networks
 
-data_path = 
-files = dir(fullfile('MyFolder','*. mat'));
+data_path = '/Volumes/My Passport/Curiosity/Data/Processed';
+files = dir(fullfile(data_path, '*.mat'));
