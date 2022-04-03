8322f155c56f3ae6eb648e64feb6daf8083ca0dd
Shubhankar <spatank@seas.upenn.edu>
2022-04-03 19:58:50 -0400

    Pre-submission code

--- Helper/network_size.m
+++
@@ -1,9 +0,0 @@
-function [num_nodes, num_edges] = network_size(adj)
-% Add detail
-adj = double(adj);
-adj(adj == 0) = NaN;
-edg = Adj2Edg(adj);
-num_nodes = size(adj, 1);
-num_edges = length(edg)/2;
-end
-
